fd44802da18336dbb39ac27edfb9ece71d1fb7e1
CheSubhro <mitsubhra@gmail.com>
2026-07-23 21:50:41 +0530

    feat: initialize project with React, Vite, Tailwind CSS, and HeroUI v3 setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import { ErrorBoundary } from './components/common';
-import MainLayout from './layouts/MainLayout';
+import { Button } from "@heroui/react";
 
-function App() {
+export default function App() {
     return (
-        <ChakraProvider value={defaultSystem}>
-            <Router>
-                <ErrorBoundary>
-                    <MainLayout>
-                        {/* <Home /> */}
-                        <h1>Welcome to CheSubhro's App</h1>
-                    </MainLayout>
-                </ErrorBoundary> 
-            </Router>
-        </ChakraProvider>
-    )
+        <div className="flex justify-center items-center h-screen">
+            <Button color="primary">HeroUI Working!</Button>
+        </div>
+    );
 }
-
-export default App
